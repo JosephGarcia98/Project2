73076a55bc0a83e8340da2d744a6545e37d787c1
JosephGarcia98 <garciajoseph998@yahoo.com>
2024-07-30 00:22:28 -0400

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,35 @@
+function openBoxing(){
+	window.open("https://olympics.com/en/paris-2024/schedule/boxing");
+}
+
+function openBasketball(){
+	window.open("https://olympics.com/en/paris-2024/schedule/basketball");
+}
+
+function openFootball(){
+	window.open("https://olympics.com/en/paris-2024/schedule/football");
+}
+
+function openJudo(){
+	window.open("https://olympics.com/en/paris-2024/schedule/judo");
+}
+
+function openTennis(){
+	window.open("https://olympics.com/en/paris-2024/schedule/tennis");
+}
+
+function openNews1(){
+	window.open("https://olympics.com/en/news/paris-2024-swimming-romania-popovici-gold-200m-freestyle-x1645");
+}
+
+function openNews2(){
+	window.open("https://olympics.com/en/news/skateboard-jagger-eaton-silver-medal-paris-2024");
+}
+
+function openNews3(){
+	window.open("https://olympics.com/en/news/paris-2024-skateboard-street-men-horigome-eaton-huston");
+}
+
+function openNewsAll(){
+	window.open("https://olympics.com/en/paris-2024/news");
+}
